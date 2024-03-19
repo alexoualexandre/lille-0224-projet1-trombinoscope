6d88639ea21931c955110400397dfb08264fa836
iSayZ <estrine.alexis@gmail.com>
2024-03-19 14:53:22 +0100

    clean code and files, add folder for profile picture

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
+import profileArray from "./profileArray.json" assert { type: "json" };
+
 let button = document.getElementById('button');
+let btnMoreCreator = document.querySelectorAll("footer span");
+let btnExit = document.querySelector(".button-exit");
+
+let cardContainer = document.querySelector(".container-card");
+let cardPicture = document.querySelector(".picture-card");
+let cardAge = document.querySelector(".age-card");
+let cardName = document.querySelector(".name-card");
+let cardFunction = document.querySelector(".function-card");
+let cardHobbie = document.querySelector(".hobbie-card");
+let cardGithub = document.querySelector(".github");
+let cardLinkedin = document.querySelector(".linkedin");
+
+let directionContainer = document.querySelector("#direction .container-profil");
+let devContainer = document.querySelector("#dev .container-profil");
+let dataContainer = document.querySelector("#data .container-profil");
+
+
+createProfilSection(profileArray);
+
+
 setInterval(function () {
     window.addEventListener('scroll', function (e) {
         if (window.scrollY > 50) {
@@ -7,120 +29,96 @@ setInterval(function () {
             button.style.display = "none";
         }
 
-    })
+    });
 }, 10);
 
-// CREATION DES PROFILS
-
-let directionContainer = document.querySelector("#direction .container-profil")
-let devContainer = document.querySelector("#dev .container-profil")
-let dataContainer = document.querySelector("#data .container-profil")
-
-
-let directionProfil = [
-    { id: "0", sexe: "m", srcPicture: "img/profil0.jpg", firstname: "Tom", lastname: "Monteil", function: "Owner", age: "30", hobbie: "Cinéma" },
-    { id: "1", sexe: "f", srcPicture: "img/profil1.jpg", firstname: "Sophie", lastname: "Delacroix", function: "Co-Owner", age: "30", hobbie: "Cinéma" },
-    { id: "2", sexe: "m", srcPicture: "img/profil2.jpg", firstname: "Pierrick", lastname: "Choffard", function: "CEO", age: "30", hobbie: "Cinéma" },
-    { id: "3", sexe: "m", srcPicture: "img/profil3.jpg", firstname: "Hugo", lastname: "Girardot", function: "Director", age: "30", hobbie: "Cinéma" }
-]
-
-let devProfil = [
-    { id: "0", sexe: "f", srcPicture: "img/profil4.jpg", firstname: "Linda", lastname: "Chagnon", function: "Personnel Manager", age: "30", hobbie: "Cinéma" },
-    { id: "1", sexe: "m", srcPicture: "img/profil5.jpg", firstname: "William", lastname: "Chandonnet", function: "Finance Manager", age: "30", hobbie: "Cinéma" },
-    { id: "2", sexe: "f", srcPicture: "img/profil6.jpg", firstname: "Sarah", lastname: "Hardouin", function: "Marketing Manager", age: "30", hobbie: "Cinéma" },
-    { id: "3", sexe: "m", srcPicture: "img/profil7.jpg", firstname: "Romain", lastname: "Garcia", function: "Purchasing Manager", age: "30", hobbie: "Cinéma" },
-    { id: "4", sexe: "f", srcPicture: "img/profil8.jpg", firstname: "Ane", lastname: "Lapierre", function: "Production Manager", age: "30", hobbie: "Cinéma" },
-    { id: "5", sexe: "f", srcPicture: "img/profil9.jpg", firstname: "Lisa", lastname: "Melanson", function: "Sales Manager", age: "30", hobbie: "Cinéma" },
-    { id: "6", sexe: "m", srcPicture: "img/profil10.jpg", firstname: "Vincent", lastname: "Truchon", function: "R&D Manager", age: "30", hobbie: "Cinéma" }
-]
-
-let dataProfil = [
-    { id: "0", sexe: "f", srcPicture: "img/profil4.jpg", firstname: "Linda", lastname: "Chagnon", function: "Personnel Manager", age: "30", hobbie: "Cinéma" },
-    { id: "1", sexe: "m", srcPicture: "img/profil5.jpg", firstname: "William", lastname: "Chandonnet", function: "Finance Manager", age: "30", hobbie: "Cinéma" },
-    { id: "2", sexe: "f", srcPicture: "img/profil6.jpg", firstname: "Sarah", lastname: "Hardouin", function: "Marketing Manager", age: "30", hobbie: "Cinéma" },
-    { id: "3", sexe: "m", srcPicture: "img/profil7.jpg", firstname: "Romain", lastname: "Garcia", function: "Purchasing Manager", age: "30", hobbie: "Cinéma" },
-    { id: "4", sexe: "f", srcPicture: "img/profil8.jpg", firstname: "Ane", lastname: "Lapierre", function: "Production Manager", age: "30", hobbie: "Cinéma" },
-    { id: "5", sexe: "f", srcPicture: "img/profil9.jpg", firstname: "Lisa", lastname: "Melanson", function: "Sales Manager", age: "30", hobbie: "Cinéma" },
-    { id: "6", sexe: "m", srcPicture: "img/profil10.jpg", firstname: "Vincent", lastname: "Truchon", function: "R&D Manager", age: "30", hobbie: "Cinéma" }
-]
 
 function createProfil(sector, id, srcPicture, firstname, lastname) {
-    let newProfil = document.createElement("div")
-    newProfil.classList.add("profil")
-    sector.appendChild(newProfil)
+    let newProfil = document.createElement("div");
+    newProfil.classList.add("profil");
+    sector.appendChild(newProfil);
 
-    let picture = document.createElement("img")
-    picture.classList.add("profil-picture")
-    picture.src = srcPicture
-    newProfil.appendChild(picture)
+    let picture = document.createElement("img");
+    picture.classList.add("profil-picture");
+    picture.src = `${srcPicture}.jpg`;
+    newProfil.appendChild(picture);
 
-    let name = document.createElement("h2")
-    name.classList.add("profil-name")
-    name.innerHTML = `${firstname}<br>${lastname}`
-    newProfil.appendChild(name)
+    let name = document.createElement("h2");
+    name.classList.add("profil-name");
+    name.innerHTML = `${firstname}<br>${lastname}`;
+    newProfil.appendChild(name);
 
-    let btnMore = document.createElement("button")
-    btnMore.classList.add("profil-more")
-    btnMore.setAttribute("id", id)
-    btnMore.textContent = "En savoir plus"
-    newProfil.appendChild(btnMore)
+    let btnMore = document.createElement("button");
+    btnMore.classList.add("profil-more");
+    btnMore.textContent = "En savoir plus";
+    btnMore.addEventListener('click', () => {
+        searchProfil(id, profileArray);
+    });
+    newProfil.appendChild(btnMore);
 }
 
-function createProfilSection(section, array) {
-    for (let i = 0; i < array.length; i++) {
-        createProfil(section, array[i].id, array[i].srcPicture, array[i].firstname, array[i].lastname)
+function createProfilSection(profileArray) {
+    profileArray.map((profile) => {
+        if(profile.dev){
+            createProfil(devContainer, profile.id, profile.srcPicture, profile.firstname, profile.lastname);
+        } else if (profile.direction) {
+            createProfil(directionContainer, profile.id, profile.srcPicture, profile.firstname, profile.lastname);
+        } else if (profile.data) {
+            createProfil(dataContainer, profile.id, profile.srcPicture, profile.firstname, profile.lastname);
+        }
+    });
+}
+
+
+function searchProfil(id, profileArray) {
+    const result = profileArray.find((profil) => profil.id === id);
+    const pictureCardContainer = document.querySelector('.picture-card-container');
+    const numberOfImages = pictureCardContainer.children.length;
+    if(numberOfImages === 2) {
+        const looserImageRef = document.querySelector('.looser-image');
+        looserImageRef.style.display = 'none';
+    }
+    cardContainer.style.display = "flex";
+    cardPicture.style.display = 'block';
+    cardPicture.src = `${result.srcPicture}.jpg`;
+    cardName.innerHTML = `${result.firstname}<br>${result.lastname}`;
+    cardAge.innerHTML = `${result.age} ans`;
+    cardFunction.innerHTML = result.sexe === "m" ? `Ancien ${result.function}` : `Ancienne ${result.function}`;
+    cardHobbie.innerHTML = `Hobbie : ${result.hobbie}`;
+    cardGithub.href = result.github;
+    cardGithub.style.display = !result.github ? "none" : "inline-block";
+    cardLinkedin.href = result.linkedin;
+    cardLinkedin.style.display = !result.linkedin ? "none" : "inline-block";
+    if(id === "4") {
+        const numberOfImages = pictureCardContainer.children.length;
+        cardPicture.style.display = 'none';
+        if(numberOfImages === 1) {
+            let looserImage = document.createElement("img");
+            looserImage.classList.add('looser-image');
+            looserImage.classList.add('picture-card');
+            pictureCardContainer.appendChild(looserImage);
+            const looserImageRef = document.querySelector('.looser-image');
+            looserImageRef.src = `${result.srcPicture}.jpg`;
+            let music = document.createElement("div");
+            looserImageRef.addEventListener("mouseover", () => {
+                music.innerHTML = `<audio src="test.mp3" autoplay="true"></audio>`;
+                looserImageRef.src = `${result.srcPicture}bis.jpg`;
+                looserImageRef.classList.add("movePicture");
+                cardContainer.appendChild(music);
+            })
+            looserImageRef.addEventListener("mouseleave", () => {
+                looserImageRef.src = `${result.srcPicture}.jpg`;
+                looserImageRef.classList.remove("movePicture");
+                cardContainer.removeChild(music);
+            })
+        } else if (numberOfImages === 2) {
+            const looserImageRef = document.querySelector('.looser-image');
+            looserImageRef.style.display = 'block';
+        }
     }
 }
 
-createProfilSection(directionContainer, directionProfil)
-createProfilSection(devContainer, devProfil)
-createProfilSection(dataContainer, dataProfil)
-
-// AFFICHAGE CARD
-
-let btnMoreDirection = document.querySelectorAll("#direction .profil-more")
-let btnMoreDev = document.querySelectorAll("#dev .profil-more")
-let btnMoreData = document.querySelectorAll("#data .profil-more")
-let btnExit = document.querySelector(".button-exit")
-
-let cardContainer = document.querySelector(".container-card")
-let cardPicture = document.querySelector(".picture-card")
-let cardAge = document.querySelector(".age-card")
-let cardName = document.querySelector(".name-card")
-let cardFunction = document.querySelector(".function-card")
-let cardHobbie = document.querySelector(".hobbie-card")
-
-
-function searchProfil(profil, array) {
-    const profilNumber = profil
-    const result = array.find((profil) => profil.id === profilNumber)
-    cardContainer.style.display = "flex"
-    cardPicture.src = result.srcPicture
-    cardName.innerHTML = `${result.firstname}<br>${result.lastname}`
-    cardAge.innerHTML = `${result.age} ans`
-    cardFunction.innerHTML = result.sexe === "m" ? `Ancien ${result.function}` : `Ancienne ${result.function}`
-    cardHobbie.innerHTML = `Hobbie : ${result.hobbie}`
-}
-
-for (let btn of btnMoreDirection) {
-    btn.addEventListener("click", () => {
-        searchProfil(btn.id, directionProfil)
-    })
-}
-
-for (let btn of btnMoreDev) {
-    btn.addEventListener("click", () => {
-        searchProfil(btn.id, devProfil)
-    })
-}
-
-for (let btn of btnMoreData) {
-    btn.addEventListener("click", () => {
-        searchProfil(btn.id, dataProfil)
-    })
-}
 
 btnExit.onclick = function () {
-    cardContainer.style.display = "none"
+    cardContainer.style.display = "none";
 }
-
